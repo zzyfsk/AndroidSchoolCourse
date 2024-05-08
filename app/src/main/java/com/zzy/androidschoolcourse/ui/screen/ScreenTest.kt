2831cb83aa1bd2f95f78c533995916fa5761afc6
zzyfsk <zzy_dada@Outlook.com>
2024-05-08 16:15:48 +0800

    5-8 test

--- app/src/main/java/com/zzy/androidschoolcourse/ui/screen/ScreenTest.kt
+++ app/src/main/java/com/zzy/androidschoolcourse/ui/screen/ScreenTest.kt
@@ -1,13 +1,29 @@
 package com.zzy.androidschoolcourse.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import cafe.adriel.voyager.core.screen.Screen
+import com.zzy.androidschoolcourse.R
 
 class ScreenTest:Screen {
     @Composable
     override fun Content() {
         Text("This is Test Screen")
+        Row {
+            ButtonYunSuan(id = R.drawable.add, onClick = false)
+            ButtonYunSuan(id = R.drawable.minus, onClick = false)
+            ButtonYunSuan(id = R.drawable.multiply, onClick = false)
+        }
     }
 
+    @Composable
+    fun ButtonYunSuan(id:Int,onClick:Boolean){
+        Button(onClick = { /*TODO*/ }) {
+            Image(painter = painterResource(id = id), contentDescription = "")
+        }
+    }
 }
