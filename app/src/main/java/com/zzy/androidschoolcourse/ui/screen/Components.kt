43cd4cf139e7daab501f489f5c141cc25a10f4ba
zzyfsk <zzy_dada@Outlook.com>
2024-08-28 18:46:31 +0800

    8-28 修改应用图标

--- app/src/main/java/com/zzy/androidschoolcourse/ui/screen/Components.kt
+++ app/src/main/java/com/zzy/androidschoolcourse/ui/screen/Components.kt
@@ -1,25 +1,29 @@
 package com.zzy.androidschoolcourse.ui.screen
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.zzy.component.background.CanvasWave
 
 @Composable
 fun CircularImageButton(
     modifier: Modifier = Modifier,
     size: Dp = 48.dp,
-    imageId:Int = ResourceConstants.ADD_IMAGE_ID,
+    imageId: Int = ResourceConstants.ADD_IMAGE_ID,
     onClick: () -> Unit = {}
 ) {
     IconButton(
@@ -27,26 +31,40 @@ fun CircularImageButton(
         onClick = onClick
     ) {
         Box(modifier = Modifier.fillMaxSize()) {
-            DrawCircleWithImage(size = size, imagePainter = painterResource(id = imageId))
+            DrawCircleWithImage(imagePainter = painterResource(id = imageId))
         }
     }
 }
 
 @Composable
 fun DrawCircleWithImage(
-    size: Dp,
-    imagePainter: Painter,
-    color: Color = Color.White
+    modifier: Modifier = Modifier,
+    size: Dp = 50.dp,
+    imagePainter: Painter
 ) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            drawCircle(color, radius = size.toPx() / 2)
-        }
+    Box(modifier = modifier) {
         Image(
             painter = imagePainter,
             contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
+            modifier = modifier.size(size = size),
             contentScale = ContentScale.Fit
         )
+    }
+}
+
+@Composable
+fun BoxWave(
+    modifier: Modifier = Modifier,
+    height: Dp = 100.dp,
+    color:Color = MaterialTheme.colorScheme. primary,
+    content: @Composable () -> Unit = {}
+) {
+    val widthX = LocalConfiguration.current.screenWidthDp
+    val width = (widthX + 1).dp
+    Box(modifier = modifier) {
+        CanvasWave(modifier = Modifier.fillMaxWidth(), height = height, width = width, color = color)
+        Column(modifier = Modifier.fillMaxSize()) {
+            content()
+        }
     }
 }
